1cfd2ef30d11f6a5db16399486d54d7218eec6f5
Sujal Gupta <sujalgupta6100@gmail.com>
2023-09-01 00:42:19 +0530

    :

---
+++ src/weatherService.js
@@ -0,0 +1,50 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const makeIconURL = (iconId) =>
+  `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+  if(city.length == 0){
+    city = "Delhi"
+  }
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+  let data;
+  try{
+      data = await fetch(URL)
+      .then((res)=>res.json())
+      .then((data)=>data)
+      console.log(data)
+  }catch(err){
+    console.log(err)
+  }
+  // const data = await fetch(URL)
+  //   .then((res) => res.json())
+  //   .then((data) => data);
+  //   console.log(data)
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, humidity, pressure },
+    sys: { country },
+    wind: { speed },
+    name,
+    message,
+  } = data;
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    pressure,
+    country,
+    speed,
+    name,
+    message,
+  };
+};
+
+export { getFormattedWeatherData };
